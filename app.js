01aada3356058902c61620999c9d42474160ba53
Rodrigo Alfaia <revaristo@gmail.com>
2025-02-15 19:03:18 -0300

    arquivos

---
+++ app.js
@@ -0,0 +1,42 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomes =[];
+
+function limparCampo(){
+    campo=document.querySelector('input');
+    campo.value="";
+}
+
+
+
+function exibirTexto(id,text){
+    let campo=document.getElementById(id);
+    campo.innerHTML=text;
+}
+
+function adicionarAmigo(){
+    let amigoAdd=document.querySelector('input').value;
+    if(amigoAdd==""){
+        alert("Digite o nome de um amigo");
+    }else{
+        nomes.push(amigoAdd);
+        var i;
+      
+       for (i=0;i<nomes.length;i++){
+       item=nomes[i];
+       exibirTexto(id='listaAmigos',text= item);
+       
+       ;
+}
+       
+        limparCampo();
+    }
+    console.log(nomes,item);
+    
+}
+function sortearAmigo(){
+    let nomesInserido = nomes.length;
+    let nomeEscolhido= parseInt(Math.random() * nomesInserido);
+    console.log(nomeEscolhido);
+    exibirTexto(id='resultado',text= 'O meu amigo secreto é ' + nomes[nomeEscolhido]); 
+
+}
